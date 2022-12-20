feba22178f21ade905618837670762e520dae369
AndrAnn <andreeva.anva@gmail.com>
2022-12-20 11:49:44 +0300

    if-else

---
+++ script2.js
@@ -0,0 +1,10 @@
+"use strict";
+let result = prompt("Введи число");
+if(result > 0){
+    alert ("1, значение больше нуля");
+} else if (result < 0){
+    alert ("-1, значение меньше нуля");
+} else {
+    alert ("0, значение == нулю");
+}
+
